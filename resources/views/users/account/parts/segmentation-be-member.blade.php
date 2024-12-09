da60951bff0dc301ab938165dfe119d6fe0f3f5b
Robin Duqué <robin.duque@outlook.com>
2024-12-09 14:38:59 +0100

    made become_member required

--- resources/views/users/account/parts/segmentation-be-member.blade.php
+++ resources/views/users/account/parts/segmentation-be-member.blade.php
@@ -1,7 +1,6 @@
 <div>
-    <label component="custom-checkbox" class="toggle-switch @if($errors->has('become_member')) text-neg @endif">
-        <input type="checkbox" name="become_member" value="1" @if(getValFromUserSettingOrOld('become_member') === '1') checked @endif  @if($disabled ?? false) disabled="disabled" @endif>
-        <span tabindex="0" role="checkbox" aria-checked="{{ getValFromUserSettingOrOld('become_member') == '1' ? 'true' : 'false' }}" class="custom-checkbox text-primary">@icon('check')</span>
+    <label component="custom-checkbox" for="{{ $id ?? 'become_member' }}" class="tw-mt-3 tw-flex tw-items-center tw-gap-3" @if($errors->has('become_member')) text-neg @endif">
+        <input type="checkbox" id="{{ $id ?? 'become_member' }}" required name="become_member" value="1" @if(getValFromUserSettingOrOld('become_member') === '1') checked @endif  @if($disabled ?? false) disabled="disabled" @endif>
         <span class="label">{{trans('segmentation.become_member')}}</span>
     </label>
     @include('users.account.parts.error', ['name' => 'become_member'])
